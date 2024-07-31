be2b33dbf2d15827538c0820e7481cf27249a4ee
ksiri <ksiri@sertiscorp.com>
2024-07-31 15:47:02 +0700

    first commit

---
+++ config/config.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"go-crud/utils"
+
+	"github.com/joho/godotenv"
+)
+
+type (
+	Config struct {
+		DBType string
+		Server *Server
+		Db     *Db
+	}
+
+	Server struct {
+		Port int64
+	}
+
+	Db struct {
+		MongoDBURI  string
+		PostgresDSN string
+	}
+
+	// Db struct {
+	// 	Host     string
+	// 	Port     int64
+	// 	User     string
+	// 	Password string
+	// 	DBName   string
+	// 	SSLMode  string
+	// 	Timezone string
+	// }
+)
+
+func LoadConfig(path string) *Config {
+	if err := godotenv.Load(path); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return &Config{
+		DBType: os.Getenv("DB_TYPE"),
+		Server: &Server{
+			Port: utils.ParseStringToInt(os.Getenv("SERVER_PORT")),
+		},
+		Db: &Db{
+			MongoDBURI:  os.Getenv("DB_MONGODB_URI"),
+			PostgresDSN: os.Getenv("DB_PostgresDSN"),
+		},
+	}
+}
